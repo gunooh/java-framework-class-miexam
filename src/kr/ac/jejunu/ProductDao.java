7a35e7165d7f8d8cb9e69d7ab6539c51f4d863fb
gunooh <gunooh12@gmail.com>
2017-04-21 23:02:31 +0900

    14

--- src/kr/ac/jejunu/ProductDao.java
+++ src/kr/ac/jejunu/ProductDao.java
@@ -1,56 +1,59 @@
 package kr.ac.jejunu;
 
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
-import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 import java.sql.*;
-import java.util.Random;
 
 public class ProductDao {
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
     public ProductDao() {
     }
 
     public Product get(final Long id) throws ClassNotFoundException, SQLException {
-        return jdbcContext.jdbcContextWithStatementStrategyForQuery(new StatementStrategy() {
-            String query = "select * from product where id = ?";
-            Object[] params = {id};
-            @Override
-            public PreparedStatement makeStatement(Connection connection) throws SQLException, SQLException {
-                PreparedStatement preparedStatement = connection.prepareStatement(query);
-                for(int i=1; i<=params.length; i++)
-                {
-                    preparedStatement.setObject(i, params[i-1]);
-                }
-                return preparedStatement;
-            }
-        });
+        String sql = "select * from product where id = ?";
+        Object[] params = new Long[] {id};
+        Product queryForObject = null;
+        try {
+            queryForObject = jdbcTemplate.queryForObject(sql, params, (resultSet, i) -> {
+
+                    Product product = new Product();
+                    product.setId(resultSet.getLong("id"));
+                    product.setTitle(resultSet.getString("title"));
+                    product.setPrice(resultSet.getInt("price"));
+                    return product;
+            });
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+        return queryForObject;
     }
 
     public void add(final Product product) throws SQLException, ClassNotFoundException {
         final String query = "insert into product(id, title, price) values(?, ?, ?)";
         final Object[] params = {product.getId(), product.getTitle(), product.getPrice()};
 
-        jdbcContext.update(query, params);
+        jdbcTemplate.update(query, params);
     }
 
     public void delete(final Long id) throws SQLException {
         final String query = "delete from product where id = ?";
         final Object[] params = {id};
 
-        jdbcContext.update(query, params);
+        jdbcTemplate.update(query, params);
     }
 
     public void update(final Product product) throws SQLException {
         final String query = "update product set title = ?, price = ? where id =?";
         final Object[] params = {product.getTitle(), product.getPrice(), product.getId()};
 
-        jdbcContext.update(query, params);
+        jdbcTemplate.update(query, params);
     }
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 }
