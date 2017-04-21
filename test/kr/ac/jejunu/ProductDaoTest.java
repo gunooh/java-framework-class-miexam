eaab24d8d88842196f921a2343b7f94a28495ef5
gunooh <gunooh12@gmail.com>
2017-04-21 16:50:13 +0900

    01

--- test/kr/ac/jejunu/ProductDaoTest.java
+++ test/kr/ac/jejunu/ProductDaoTest.java
@@ -3,6 +3,7 @@ package kr.ac.jejunu;
 import org.junit.Test;
 
 import java.sql.SQLException;
+import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,5 +20,26 @@ public class ProductDaoTest {
         assertThat(id, is(product.getId()));
         assertThat(title, is(product.getTitle()));
         assertThat(price, is(product.getPrice()));
+    }
+
+    public void add() throws SQLException, ClassNotFoundException {
+        Long id = Long.valueOf(new Random().nextInt());
+        String title = "제주감귤";
+        Integer price = 15000;
+
+        Product product = new Product();
+        product.setId(id);
+        product.setTitle(title);
+        product.setPrice(price);
+
+        ProductDao productDao = new ProductDao();
+
+        productDao.add(product);
+
+        Product addedProduct = productDao.get(id);
+
+        assertThat(id, is(addedProduct.getId()));
+        assertThat(title, is(addedProduct.getTitle()));
+        assertThat(price, is(addedProduct.getPrice()));
     }
 }
